ce38249b56011c6c33ac42f1d667287b58a09fd0
cpan001 <github email address>
2017-10-22 15:30:48 -0400

    Done.

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -2,13 +2,13 @@ module Api
   module V1
     class CatsController < ApplicationController
 
-      def index
-        render json: Cat.includes(:hobbies), include: ['hobbies']
-      end
+       def index
+         render json: Cat.includes(:hobbies), include: ['hobbies']
+       end
 
-      def show
-        render json: Cat.find(params[:id]), include: ['hobbies']
-      end
+       def show
+         render json: Cat.find(params[:id]), include: ['hobbies']
+       end
     end
   end
 end
